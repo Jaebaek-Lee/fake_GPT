35bf80d3f24534d571fc6bb2a86e4f2fa80f6591
ThinkingVincent <dlwoqor0529@gmail.com>
2023-11-27 19:46:29 +0900

    add spinner

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import config from "./apiKey.js";
 
-const button = document.querySelector("button");
+const chatButton = document.querySelector("#chat-button");
 const chatText = document.querySelector("input");
 const titleImage = document.querySelector("#title-image");
 const title = document.querySelector("#title");
@@ -109,7 +109,7 @@ async function fetchAIResponse(prompt) {
       Authorization: `Bearer ${config.apikey}`,
     },
     body: JSON.stringify({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: [
         {
           role: "system",
@@ -159,21 +159,21 @@ chatText.addEventListener("input", async (e) => {
   if (checker === 0) return;
   if (chatText.value === "") {
     // console.log(1);
-    button.innerHTML = `<img src="./img/button.png" />`;
-    button.style.pointerEvents = "none";
-    button.style.userSelect = "none";
-    button.disabled = true;
+    chatButton.innerHTML = `<img src="./img/button.png" />`;
+    chatButton.style.pointerEvents = "none";
+    chatButton.style.userSelect = "none";
+    chatButton.disabled = true;
   }
   // console.log(1);
   else {
-    button.innerHTML = `<img src="./img/button_active.png" />`;
-    button.style.pointerEvents = "auto";
-    button.style.userSelect = "auto";
-    button.disabled = false;
+    chatButton.innerHTML = `<img src="./img/button_active.png" />`;
+    chatButton.style.pointerEvents = "auto";
+    chatButton.style.userSelect = "auto";
+    chatButton.disabled = false;
   }
 });
 
-button.addEventListener("click", async (e) => {
+chatButton.addEventListener("click", async (e) => {
   e.preventDefault();
   if (chatText.value === "") {
     console.log(1);
@@ -190,20 +190,60 @@ button.addEventListener("click", async (e) => {
   addMessage("나", message);
   chatText.value = "";
 
-  button.innerHTML = `<img src="./img/spinner.png" style="width: 20px; height: 20px; margin-top: 2px;"/>`;
+  chatButton.innerHTML = `<img src="./img/spinner.png" style="width: 20px; height: 20px; margin-top: 2px;"/>`;
   // button.style.pointerEvents = "none";
   // button.style.userSelect = "none";
   // button.disabled = true;
   checker = 0;
 
+  const tempMessageElement = addTemporaryMessage();
+
   const aiResponse = await fetchAIResponse(message);
 
   // button.innerHTML = `Send`;
-  button.innerHTML = `<img src="./img/button.png" />`;
-  button.style.pointerEvents = "none";
-  button.style.userSelect = "none";
-  button.disabled = true;
+  chatButton.innerHTML = `<img src="./img/button.png" />`;
+  chatButton.style.pointerEvents = "none";
+  chatButton.style.userSelect = "none";
+  chatButton.disabled = true;
   checker = 1;
 
+  // 임시 메시지 요소를 제거하고, 실제 응답으로 메시지 추가
+  chatContainer.removeChild(tempMessageElement);
   addMessage("금쪽이", aiResponse);
 });
+
+const menuButton = document.querySelector("#menu-button");
+
+menuButton.addEventListener("mouseover", async (e) => {
+  e.preventDefault();
+  menuButton.innerHTML = `<img src="./img/menu-button-active.png" class="menu-button-icon" />`;
+});
+
+menuButton.addEventListener("mouseleave", async (e) => {
+  e.preventDefault();
+  menuButton.innerHTML = `<img src="./img/menu-button-nonactive.png" class="menu-button-icon" />`;
+});
+
+const addTemporaryMessage = () => {
+  const tempMessageElement = document.createElement("div");
+  tempMessageElement.className = "chat gpt-chat";
+  const imgElement = document.createElement("img");
+  imgElement.src = "./img/gpt4-icon.png";
+  imgElement.className = "profile";
+  const nameElement = document.createElement("div");
+  nameElement.textContent = "ChatGPT";
+  nameElement.className = "chat-name";
+  const logElement = document.createElement("div");
+  logElement.className = "chat-log";
+  const spinnerElement = document.createElement("img");
+  spinnerElement.src = "./img/ellipse-spinner.png"; // 스피너 이미지 경로를 지정하세요
+  spinnerElement.className = "spinner";
+
+  logElement.appendChild(spinnerElement);
+  tempMessageElement.appendChild(imgElement);
+  tempMessageElement.appendChild(nameElement);
+  tempMessageElement.appendChild(logElement);
+
+  chatContainer.prepend(tempMessageElement);
+  return tempMessageElement;
+};
